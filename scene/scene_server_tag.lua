91f75da03f0cbe20b39ae26e36402589880d4d21
xiaobuding <1705825390@qq.com>
2024-11-25 19:16:05 +0800

    #初始地图场景更新

--- scene/scene_server_tag.lua
+++ scene/scene_server_tag.lua
@@ -1,8 +1,8 @@
 local scene_server_tag = {}
+scene_server_tag['new_scene_2'] = 'default'
 scene_server_tag['副本场景1'] = 'default'
 scene_server_tag['new_scence_2'] = 'default'
-scene_server_tag['new_scene'] = 'default'
 scene_server_tag['new_scene_1'] = 'default'
-scene_server_tag['new_scene_2'] = 'default'
+scene_server_tag['new_scene'] = 'default'
 scene_server_tag['default'] = 'default'
 return scene_server_tag
--- ui/script/scene/default/area_save.lua
+++ ui/script/scene/default/area_save.lua
@@ -5,8 +5,8 @@ local present = _G.present['default']
 present.point["Point_0"] = base.scene_point(10240.0, 3360.0, nil, "default")
 present.circle["Circles_0"] = base.circle(base.point(8848.0, 4880.0), 176.0, "default")
 present.circle["Circles_1"] = base.circle(base.point(8912.0, 7568.0), 176.0, "default")
-present.rank["rank"] = {["Point_0"] = "初始地图/27", ["Circles_0"] = "初始地图/28", ["Circles_1"] = "初始地图/29", }
+present.rank["rank"] = {["Point_0"] = "初始地图/14", ["Circles_0"] = "初始地图/15", ["Circles_1"] = "初始地图/16", }
 present.description["description"] = {["Point_0"] = "", ["Circles_0"] = "", ["Circles_1"] = "", }
-present.invisible["invisible"] = {["Point_0"] = "false", ["Circles_0"] = "", ["Circles_1"] = "", }
+present.invisible["invisible"] = {["Point_0"] = "", ["Circles_0"] = "", ["Circles_1"] = "", }
 present.unselectable["unselectable"] = {["Point_0"] = "", ["Circles_0"] = "", ["Circles_1"] = "", }
 present.link["link"] = {}
